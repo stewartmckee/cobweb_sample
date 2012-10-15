2fffb620e898c2e54fa75b7a6332243b3c5d39fc
Stewart McKee <stewart@theizone.co.uk>
2012-10-15 02:44:58 +0100

    added redis options

--- lib/crawler_resque.rb
+++ lib/crawler_resque.rb
@@ -7,7 +7,8 @@ class CrawlerResque
     cobweb_hash = {
       :processing_queue => "CrawlerResqueJob",  # the name of the class being used for the processing queue
       :crawl_finished_queue => "CrawlerFinishedJob", # the name of the class to be sent statistics when the crawl completes
-      :internal_urls => ["https://github.com/stewartmckee/cobweb/blob/master/*"] # an array of the urls regarded as internal (defaults to anything in the host and domain)
+      :internal_urls => ["https://github.com/stewartmckee/cobweb/blob/master/*"], # an array of the urls regarded as internal (defaults to anything in the host and domain)
+      :redis_options => {:host => "localhost", :port => "6379"} # these are the default values, as this option is not required, but is here if you wish to alter it to a remote redis box
     }
 
     cobweb = Cobweb.new(cobweb_hash) # creates a new cobweb object with the settings required
